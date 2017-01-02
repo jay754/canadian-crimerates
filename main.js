03ffc0249a64e7c199def2428ae6761fe00fcbdf
Jay Engineer <jayengineer6@gmail.com>
2017-01-01 23:33:46 -0500

    added the pie graph for homicides

---
+++ main.js
@@ -0,0 +1,94 @@
+/*
+
+Main.js
+
+*/
+
+$(function(){
+
+  function getData(callback){
+    $.ajax({
+      url: "2015/crime_stats_canada.json",
+      type: "GET",
+      dataType: "json",
+      success: function(data) {
+        callback(data);
+      },
+      error: function(xhr, ajaxOptions, thrownError){
+        console.log(xhr.status);
+        console.log(thrownError);
+      }
+    });
+  }
+
+  function generatePiegraph(){
+    getData(function(results){
+      var data = results[4];
+      var content = [];
+
+      for(var key in data){
+        var value = data[key];
+        if(key !== "Title" && key !== "Canada"){
+          content.push({
+            "label":key,
+            "value":value,
+          });
+        }
+      }
+
+      var pie = new d3pie("pieChart", {
+        "size": {
+          "canvasWidth": 900,
+          "canvasHeight": 600,
+        },
+        "data": {
+          "sortOrder": "label-desc",
+
+          "content": content
+        },
+        "labels": {
+          "outer": {
+            "format": "label-percentage1",
+            "pieDistance": 10
+          },
+          "inner": {
+            "hideWhenLessThanPercentage": 100
+          },
+          "mainLabel": {
+            "fontSize": 11
+          },
+          "percentage": {
+            "color": "#000000",
+            "fontSize": 15,
+            "decimalPlaces": 2
+          },
+          "value": {
+            "color": "#cccc43",
+            "fontSize": 11
+          },
+          "lines": {
+            "enabled": true,
+            "color": "#777777"
+          },
+          "truncation": {
+            "enabled": true
+          }
+        },
+        "effects": {
+          "pullOutSegmentOnClick": {
+            "effect": "linear",
+            "speed": 400,
+            "size": 8
+          }
+        },
+        "misc": {
+          "colors": {
+            "segmentStroke": "#000000"
+          }
+        }
+      });
+    });
+  }
+
+  generatePiegraph();
+});
