5b2e79e694878e20c8c9f483a59276f8bd3802e8
Jay Engineer <jayengineer6@gmail.com>
2017-01-02 19:22:59 -0500

    final change

--- main.js
+++ main.js
@@ -16,67 +16,67 @@ $(function(){
 
   /* For the Bar Chart */
 
-  // function drawChart(){
-  //   // set the dimensions and margins of the graph
-  //   var margin = {top: 20, right: 20, bottom: 30, left: 40},
-  //       width = 960 - margin.left - margin.right,
-  //       height = 500 - margin.top - margin.bottom;
-  //
-  //   // set the ranges
-  //   var x = d3.scaleBand()
-  //             .range([0, width])
-  //             .padding(0.1);
-  //   var y = d3.scaleLinear()
-  //             .range([height, 0]);
-  //
-  //   // append the svg object to the body of the page
-  //   // append a 'group' element to 'svg'
-  //   // moves the 'group' element to the top left margin
-  //   var svg = d3.select("body").append("svg")
-  //       .attr("width", width + margin.left + margin.right)
-  //       .attr("height", height + margin.top + margin.bottom)
-  //       .attr("text-anchor", "middle")
-  //         .style("font-size", "16px")
-  //         .style("text-decoration", "underline")
-  //         .text("Value vs Date Graph")
-  //     .append("g")
-  //       .attr("transform",
-  //             "translate(" + margin.left + "," + margin.top + ")");
-  //
-  //   // get the data
-  //   d3.csv("2015/data.csv", function(error, data) {
-  //     if (error) throw error;
-  //
-  //     // format the data
-  //     data.forEach(function(d) {
-  //       d.Homicides = +d.Homicides;
-  //     });
-  //
-  //     // Scale the range of the data in the domains
-  //     x.domain(data.map(function(d) { return d.Province; }));
-  //     y.domain([0, d3.max(data, function(d) { return d.Homicides; })]);
-  //
-  //     // append the rectangles for the bar chart
-  //     svg.selectAll(".bar")
-  //         .data(data)
-  //       .enter().append("rect")
-  //         .attr("class", "bar")
-  //         .attr("x", function(d) { return x(d.Province); })
-  //         .attr("width", x.bandwidth())
-  //         .attr("y", function(d) { return y(d.Homicides); })
-  //         .attr("height", function(d) { return height - y(d.Homicides); });
-  //
-  //     // add the x Axis
-  //     svg.append("g")
-  //         .attr("transform", "translate(0," + height + ")")
-  //         .call(d3.axisBottom(x));
-  //
-  //     // add the y Axis
-  //     svg.append("g")
-  //         .call(d3.axisLeft(y));
-  //
-  //   });
-  // }
+  function drawChart(){
+    // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    // set the ranges
+    var x = d3.scaleBand()
+              .range([0, width])
+              .padding(0.1);
+    var y = d3.scaleLinear()
+              .range([height, 0]);
+
+    // append the svg object to the body of the page
+    // append a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("text-anchor", "middle")
+          .style("font-size", "16px")
+          .style("text-decoration", "underline")
+          .text("Value vs Date Graph")
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+    // get the data
+    d3.csv("2015/data.csv", function(error, data) {
+      if (error) throw error;
+
+      // format the data
+      data.forEach(function(d) {
+        d.Homicides = +d.Homicides;
+      });
+
+      // Scale the range of the data in the domains
+      x.domain(data.map(function(d) { return d.Province; }));
+      y.domain([0, d3.max(data, function(d) { return d.Homicides; })]);
+
+      // append the rectangles for the bar chart
+      svg.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.Province); })
+          .attr("width", x.bandwidth())
+          .attr("y", function(d) { return y(d.Homicides); })
+          .attr("height", function(d) { return height - y(d.Homicides); });
+
+      // add the x Axis
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+      // add the y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y));
+
+    });
+  }
 
   /* For the pie Chart */
 
@@ -160,6 +160,6 @@ $(function(){
     });
   }
 
-  // drawChart();
-  drawPiegraph();
+  drawChart();
+  // drawPiegraph();
 });
